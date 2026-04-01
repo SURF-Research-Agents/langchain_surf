cf5bbd778e1c78037c84ccb301f99164343d5e65
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2026-04-01 17:33:19 +0200

    make chat

---
+++ docs/examples/chat_willma.py
@@ -0,0 +1,20 @@
+import os
+from langchain_surf import ChatWillma   
+from dotenv import load_dotenv
+
+load_dotenv('../../.env')  # Load environment variables from .env file
+
+api_key = os.getenv("AIHUB_API_KEY")
+model = "Llama 3.1 8B Instruct"
+
+
+model = ChatWillma(
+    model=model,
+    temperature=0.1,
+    max_tokens=1000,
+    timeout=30,
+    api_key=api_key,
+)
+
+result = model.invoke("What is the capital of France?")
+print(result)
--- src/langchain_surf/__init__.py
+++ src/langchain_surf/__init__.py
@@ -1,9 +1,16 @@
 """Documentation about langchain_surf."""
 
 import logging
+from .chat_models.chat_willma import ChatWillma
+from .tools.hpc_tools import tool
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Nicolas Renaud"
 __email__ = "nicolas.renaud@surf.nl"
 __version__ = "0.1.0"
+
+__all__ = [
+    "ChatWillma",
+    "tool",
+]
